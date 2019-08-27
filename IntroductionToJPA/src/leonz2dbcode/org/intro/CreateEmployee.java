4526cc558d64353ef65eb98789a4af524dffe6fc
leonz2code <leo_icom@hotmail.com>
2019-08-27 02:06:07 -0500

    FUNCTIONAL EXAMPLE OF JPA

--- IntroductionToJPA/src/leonz2dbcode/org/intro/CreateEmployee.java
+++ IntroductionToJPA/src/leonz2dbcode/org/intro/CreateEmployee.java
@@ -11,21 +11,50 @@ public class CreateEmployee {
 
    public static void main( String[ ] args ) {
    
-      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );
-      
-      EntityManager entitymanager = emfactory.createEntityManager( );
-      entitymanager.getTransaction( ).begin( );
-
-      Employee employee = new Employee( ); 
-      employee.setEid( 1201 );
-      employee.setEname( "Gopal" );
-      employee.setSalary( 40000 );
-      employee.setDeg( "Technical Manager" );
-      
-      entitymanager.persist( employee );
-      entitymanager.getTransaction( ).commit( );
-
-      entitymanager.close( );
-      emfactory.close( );
+//      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );
+//      
+//      EntityManager entitymanager = emfactory.createEntityManager( );
+//      entitymanager.getTransaction( ).begin( );
+//
+//      Employee employee = new Employee( ); 
+//      employee.setEid( 1201 );
+//      employee.setEname( "Gopal" );
+//      employee.setSalary( 40000 );
+//      employee.setDeg( "Technical Manager" );
+//      
+//      entitymanager.persist( employee );
+//      entitymanager.getTransaction( ).commit( );
+//
+//      entitymanager.close( );
+//      emfactory.close( );
+  
+   
+	//   nuevoEmployee(1355, "Leonardo Navarro", 192000, "Senior Developer LEAD");
+	   
+	   nuevoEmployee(1256, "Erika Tinoko", 40000, "Director HR");
+	   
+   
    }
+   
+   public static void nuevoEmployee(int eid, String ename, double salary, String deg) {
+	   
+	      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );	      
+	      EntityManager entitymanager = emfactory.createEntityManager( );
+	      entitymanager.getTransaction( ).begin( );
+	   
+	      
+	      Employee employee = new Employee(eid,ename,salary,deg ); 
+	      
+	      entitymanager.persist( employee );
+	      entitymanager.getTransaction( ).commit( );
+
+	      entitymanager.close( );
+	      emfactory.close( );
+	   
+	   
+   }
+   
+   
+   
+   
 }
